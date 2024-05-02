78fb27402e02e9347f5b98f76a71904eb81d8593
Narges Ghanbari <narges.ghanbari@gmail.com>
2024-05-02 23:15:33 +0300

    Add osastot page

---
+++ src/main/java/com/example/kahvikauppa/OsastoController.java
@@ -0,0 +1,33 @@
+package com.example.kahvikauppa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class OsastoController {
+
+  @Autowired
+  private OsastoRepository osastoRepository;
+
+  @GetMapping("/osastot")
+    public String list(Model model) {
+        model.addAttribute("osastot", osastoRepository.findAll());
+
+        return "osastot";
+    }
+
+  @PostMapping("/osastot")
+    public String create(@RequestParam String nimi, @RequestParam Long osastoID) {
+        Osasto osasto = new Osasto();
+        osasto.setNimi(nimi);
+        osasto.setOsastoID(osastoID);
+
+        osastoRepository.save(osasto);
+        return "redirect:/osastot";
+    }
+  
+}
