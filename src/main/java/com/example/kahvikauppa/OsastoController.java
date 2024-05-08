9cec62248f1fda08a80f2719937c28dfa5e9672f
Narges Ghanbari <narges.ghanbari@gmail.com>
2024-05-08 11:21:22 +0300

    Add edit osasto page

--- src/main/java/com/example/kahvikauppa/OsastoController.java
+++ src/main/java/com/example/kahvikauppa/OsastoController.java
@@ -1,9 +1,12 @@
 package com.example.kahvikauppa;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -28,6 +31,29 @@ public class OsastoController {
 
     osastoRepository.save(osasto);
     return "redirect:/osastot";
+  }
+
+  @GetMapping("/osasto/{id}")
+  public String osasto(@PathVariable("id") Long id, Model model) {
+
+    Osasto osasto = osastoRepository.getReferenceById(id);
+    model.addAttribute("osasto", osasto);
+
+    return "osasto";
+  }
+
+  @PostMapping("/osasto/{id}")
+  public String update(
+      @PathVariable("id") Long id,
+      @RequestParam String nimi,
+      @RequestParam Long osastoID)
+      throws IOException {
+    Osasto osasto = osastoRepository.getReferenceById(id);
+    osasto.setNimi(nimi);
+    osasto.setOsastoID(osastoID);
+
+    osastoRepository.save(osasto);
+    return "redirect:/osasto/" + id;
   }
 
 }
