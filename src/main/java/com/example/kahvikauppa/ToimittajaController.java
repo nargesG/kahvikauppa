2cbbe0ce3574ce1a858888686b83753841c0e168
Narges Ghanbari <narges.ghanbari@gmail.com>
2024-05-08 10:54:45 +0300

    Add edit and remove Toimittaja pages

--- src/main/java/com/example/kahvikauppa/ToimittajaController.java
+++ src/main/java/com/example/kahvikauppa/ToimittajaController.java
@@ -1,9 +1,12 @@
 package com.example.kahvikauppa;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,21 +17,63 @@ public class ToimittajaController {
   private ToimittajaRepository toimittajaRepository;
 
   @GetMapping("/toimittajat")
-    public String list(Model model) {
-        model.addAttribute("toimittajat", toimittajaRepository.findAll());
+  public String list(Model model) {
+    model.addAttribute("toimittajat", toimittajaRepository.findAll());
 
-        return "toimittajat";
-    }
+    return "toimittajat";
+  }
 
   @PostMapping("/toimittajat")
-    public String create(@RequestParam String nimi, @RequestParam String yhteyshenkilo, @RequestParam String yhteyshenkillonEmail) {
-        Toimittaja toimittaja = new Toimittaja();
-        toimittaja.setNimi(nimi);
-        toimittaja.setYhteyshenkilo(yhteyshenkilo);
-        toimittaja.setYhteyshenkillonEmail(yhteyshenkillonEmail);
+  public String create(@RequestParam String nimi, @RequestParam String yhteyshenkilo,
+      @RequestParam String yhteyshenkillonEmail) {
+    Toimittaja toimittaja = new Toimittaja();
+    toimittaja.setNimi(nimi);
+    toimittaja.setYhteyshenkilo(yhteyshenkilo);
+    toimittaja.setYhteyshenkillonEmail(yhteyshenkillonEmail);
 
-        toimittajaRepository.save(toimittaja);
-        return "redirect:/toimittajat";
-    }
-  
+    toimittajaRepository.save(toimittaja);
+    return "redirect:/toimittajat";
+  }
+
+  @GetMapping("/toimittaja/{id}")
+  public String toimittaja(@PathVariable("id") Long id, Model model) {
+
+    Toimittaja toimittaja = toimittajaRepository.getReferenceById(id);
+    model.addAttribute("toimittaja", toimittaja);
+
+    return "toimittaja";
+  }
+
+  @PostMapping("/toimittaja/{id}")
+  public String update(
+      @PathVariable("id") Long id,
+      @RequestParam String nimi,
+      @RequestParam String yhteyshenkilo,
+      @RequestParam String yhteyshenkillonEmail)
+      throws IOException {
+    Toimittaja toimittaja = toimittajaRepository.getReferenceById(id);
+    toimittaja.setNimi(nimi);
+    toimittaja.setYhteyshenkilo(yhteyshenkilo);
+    toimittaja.setYhteyshenkillonEmail(yhteyshenkillonEmail);
+
+    toimittajaRepository.save(toimittaja);
+    return "redirect:/toimittaja/" + id;
+  }
+
+  @GetMapping("/poista-toimittaja/{id}")
+  public String poistaVahvistus(@PathVariable("id") Long id, Model model) {
+    Toimittaja toimittaja = toimittajaRepository.getReferenceById(id);
+    model.addAttribute("toimittaja", toimittaja);
+
+    return "poista-toimittaja";
+  }
+
+  @PostMapping("/poista-toimittaja/{id}")
+  public String delete(@PathVariable("id") Long id) throws IOException {
+    Toimittaja toimittaja = toimittajaRepository.getReferenceById(id);
+    toimittajaRepository.delete(toimittaja);
+
+    return "redirect:/toimittajat";
+  }
+
 }
