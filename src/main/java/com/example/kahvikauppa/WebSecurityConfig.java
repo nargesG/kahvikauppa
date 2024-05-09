6f3f05421caa2777e0ab00432af3f4ca55e606a9
Narges Ghanbari <narges.ghanbari@gmail.com>
2024-05-09 21:15:26 +0300

    Fix image access

--- src/main/java/com/example/kahvikauppa/WebSecurityConfig.java
+++ src/main/java/com/example/kahvikauppa/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .authorizeHttpRequests((requests) -> requests
-            .requestMatchers("/", "/myymala/*", "/images/*", "/styles/*").permitAll()
+            .requestMatchers("/", "/myymala/*", "/images/*", "/styles/*", "/image/*").permitAll()
             .anyRequest().authenticated())
         .formLogin((form) -> form
             .loginPage("/login")
