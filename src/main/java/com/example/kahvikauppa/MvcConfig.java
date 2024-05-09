307bea8755f1577b430701cd8f541e40a8c44f65
Narges Ghanbari <narges.ghanbari@gmail.com>
2024-05-09 15:13:24 +0300

    Login + security + related changes

--- src/main/java/com/example/kahvikauppa/MvcConfig.java
+++ src/main/java/com/example/kahvikauppa/MvcConfig.java
@@ -4,6 +4,7 @@ import java.nio.file.Paths;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -23,5 +24,9 @@ public class MvcConfig implements WebMvcConfigurer {
 
     // Expose the upload folder to show images in the browser
     registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/" + uploadPath + "/");
+  }
+
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/login").setViewName("login");
   }
 }
